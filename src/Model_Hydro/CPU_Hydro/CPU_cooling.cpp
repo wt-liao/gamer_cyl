0067409495bd858c6406c82dde957bf88c140997
Wei-Ting Liao <weiting.natii@gmail.com>
2019-01-12 15:18:15 -0600

    change tau_dyn

--- src/Model_Hydro/CPU_Hydro/CPU_cooling.cpp
+++ src/Model_Hydro/CPU_Hydro/CPU_cooling.cpp
@@ -31,7 +31,7 @@ void CoolingFunc(real cool_rate, const real PriVar[], const real x_pos[]) {
    
    // Boltzmann R in the unit of popIII setting
    const double t_orbit     = 0.79 ;                  // outer orbital time
-   const double R           = 4.64952804093003e+0 ; 
+   const double R           = 4.64952804093003e+0 ;   // needed for converting to T (temperature)
    
    const double t_curr      = Time[0];
    const double t_relax     = 5*t_orbit ; 
@@ -45,8 +45,8 @@ void CoolingFunc(real cool_rate, const real PriVar[], const real x_pos[]) {
    const double v_abs       = SQRT( SQR(PriVar[MOMX]) + SQR(PriVar[MOMY]) + SQR(PriVar[MOMZ]) ) ;
    const double sph_rad     = SQRT( SQR(x_pos[0]) + SQR(x_pos[2]) );
    
-   //const double tau_dyn     = SQRT( CUBE(sph_rad) / GM );
-   const double tau_dyn     = sph_rad / v_abs ;
+   const double tau_dyn     = SQRT( CUBE(sph_rad) / GM );
+   //const double tau_dyn     = sph_rad / v_abs ;
    
    // cooling parameter: cooling time = beta * (dynamical time)
    const double beta        = 3.5;
